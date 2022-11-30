3a2ac7121eb9602f111fc1ea8c8dbbfc465b2d8d
VincenzoRuggiero <vincenzoru@live.it>
2022-11-30 16:09:36 +0100

    Add function that appends block to the grid

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-// Ciclo FOR per le 100 caselle
-for (i = 0; i <= 100; i++) {
-  console.log(i);
+// // Ciclo FOR per le 100 caselle
+// Collego il container dei quadrati
+let gridElem = document.getElementById("grid");
+
+function createGrid() {
+  for (i = 1; i <= 100; i++) {
+    let squareElem = document.createElement("div");
+    squareElem.classList.add("squareCSS");
+    squareElem.innerHTML = i;
+    gridElem.appendChild(squareElem);
+  }
+
+  return;
 }
